1075b03304c04771b1a4d36634e784460109417f
Rishipatel10 <rishipatel853@gmail.com>
2024-12-28 13:57:40 +0530

    Add files via upload

--- forgot_password.php
+++ forgot_password.php
@@ -17,7 +17,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Forgot Password | <?php echo $title;?></title>
+    <title>Forgot Password | <?php echo $a;?></title>
     
     <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
     <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
@@ -242,7 +242,7 @@
                 </svg>
 
               </span>
-              <span class="app-brand-text demo h3 mb-0 fw-bold">Swad Safar</span>
+              <span class="app-brand-text demo h3 mb-0 fw-bold">Vivaahmilan</span>
             </a>
           </div>
           <!-- /Logo -->
