9834fd1ae992747c9c160495b12a6a348da9ae15
Keval_bhavsar <108937141+Kevalbhavsar27@users.noreply.github.com>
2025-01-26 16:33:02 +0530

    Delete sub-community_delete.php

--- sub-community_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-        include("connection.php");
-        $ID=$_GET['sid'];
-        $str="delete from tbl_sub_community where Sub_Community_ID =".$ID;
-        mysqli_query($conn,$str);
-        header('location:subcomunnityshow.php');
-                          
-?>
