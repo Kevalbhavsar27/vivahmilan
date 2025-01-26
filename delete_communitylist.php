8567e10edc41c7495c418e0dbc58ce6a4d9560b6
Keval_bhavsar <108937141+Kevalbhavsar27@users.noreply.github.com>
2025-01-26 16:35:42 +0530

    Delete delete_communitylist.php

--- delete_communitylist.php
+++
@@ -1,8 +0,0 @@
-<?php
-        include("connection.php");
-        $ID=$_GET['cid'];
-        $str="delete from tbl_community where Community_ID =".$ID;
-        mysqli_query($conn,$str);
-        header('location:community_list.php');
-                          
-?>
