5ef8e6f62a589f8aa1246c12361a7185dce64358
Keval_bhavsar <108937141+Kevalbhavsar27@users.noreply.github.com>
2025-01-26 16:37:05 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer footer-static footer-light">
-      <p class="clearfix mb-0"><span class="float-left d-inline-block"><?php echo date("l Y")?> &copy; VIVAHMILAN</span><span class="float-right d-sm-inline-block d-none">VIVAHMILAN<i class="bx bxs-heart pink mx-50 font-small-3"></i>
-        <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="bx bx-up-arrow-alt"></i></button>
-      </p>
-    </footer>
