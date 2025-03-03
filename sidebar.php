fba7ab95367898a7d4b90457009b18f6cf406cfa
dhvanit16 <drtrivedi2000@gmail.com>
2025-03-03 11:36:38 +0530

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<div class="col-md-4 col-lg-3">
+                        <div class="db-nav">
+                            <div class="db-nav-pro"><img src="images/profiles/12.jpg" class="img-fluid" alt=""></div>
+                            <div class="db-nav-list">
+                                <ul>
+                                    <li><a href="user-dashboard.php"><i class="fa fa-tachometer" aria-hidden="true"></i>Dashboard</a></li>
+                                    <li><a href="user-profile.php"><i class="fa fa-male" aria-hidden="true"></i>Profile</a></li>
+                                    <li><a href="user-interests.php"><i class="fa fa-handshake-o" aria-hidden="true"></i>Interests</a></li>
+                                    <li><a href="user-chat.php"><i class="fa fa-commenting-o" aria-hidden="true"></i>Chat list</a></li>
+                                    <li><a href="user-plan.php" class="act"><i class="fa fa-money" aria-hidden="true"></i>Plan</a></li>
+                                    <li><a href="user-setting.php"><i class="fa fa-cog" aria-hidden="true"></i>Setting</a></li>
+                                    <li><a href="#"><i class="fa fa-sign-out" aria-hidden="true"></i>Log out</a></li>
+                                </ul>
+                            </div>
+                        </div>
+</div>
